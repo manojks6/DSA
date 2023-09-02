f0ce08b8632b2bdc6e2f91f6219283f345ce435d
Manoj KS <118463406+manojks6@users.noreply.github.com>
2023-09-02 12:43:08 +0530

    Time: 1 ms (100.00%) | Memory: 45.3 MB (19.64%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j=0;
+       for(int i: nums){
+           if(i!=0){
+               nums[j]=i;
+               j++;
+           }
+       }
+       while(j<nums.length){
+           nums[j++]=0;
+       }
+    }
+}
