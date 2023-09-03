cb84786b1709140551bb1a790b9d3e20175a9245
Manoj KS <118463406+manojks6@users.noreply.github.com>
2023-09-03 10:13:03 +0530

    Update defanging-an-ip-address.java
    Without inbuilt methods

--- 1205-defanging-an-ip-address/defanging-an-ip-address.java
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.java
@@ -4,3 +4,18 @@ class Solution {
         return address;
     }
 }
+//---------------OR----------------
+class Solution {
+    public String defangIPaddr(String address) {
+        String ans="";
+        for(int i=0;i<address.length();i++){
+            if(address.charAt(i)=='.'){
+                ans+="[.]";
+            }
+            else{
+                ans+=address.charAt(i);
+            }
+        }
+        return ans;
+    }
+}
