97db7f393008c6eacfcbbf0792dc6d66d6a96857
Manoj KS <118463406+manojks6@users.noreply.github.com>
2023-11-22 20:54:43 +0530

    Time: 19 ms (58.64%) | Memory: 43.9 MB (46.05%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,33 +1,39 @@
 class Solution {
-    public String longestPalindrome(String str) {
-		int m=0;
-		String ans="";
-		for(int i=0;i<str.length();i++){
-			String x=f(str,i,i);
-			String y=f(str,i,i+1);
-			if(x.length()>y.length() &&x.length()>m ){
-				m=Math.max(x.length(),m);
-				ans=x;
-			}
-			else if(y.length()>m){
-				m=Math.max(y.length(),m);
-				ans=y;
-			}
-		}
-		return ans;
-	}
-	static String f(String s,int i,int j){
-		while(i>=0 && j<s.length()){
-			if(s.charAt(i)==s.charAt(j)){
-				j++;
-				i--;
-			}
-			else{
-				break;
-			}
-		}
-		String x=s.substring(i+1,j);
-		return x;
-	}        
-
+    public String longestPalindrome(String s) {
+        int m=0;
+        String a="";
+        for(int i=0;i<s.length();i++){
+            String x=f(i,i,s);
+            String y=i<s.length()-1?f(i,i+1,s):"";
+            if(x.length()>y.length()){
+                if(x.length()>m){
+                    a=x;
+                    m=x.length();
+                }
+            }
+            else{
+                if(y.length()>m){
+                    a=y;
+                    m=y.length();
+                }
+            }
+        }
+        return a;
+    }
+    static String f(int i,int j,String s){
+        int ans=1;
+        while(i>=0 && j<s.length()){
+            if(s.charAt(i)==s.charAt(j)){
+                i--;
+                j++;
+            }
+            else{
+                break;
+            }
+        }
+        String a="";
+        if(i<j){
+        a=s.substring(i+1,j);}
+        return a;
+    }
 }
