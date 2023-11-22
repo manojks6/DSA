e5900a74848035cec693711d9ef616eacdae99f4
Manoj KS <118463406+manojks6@users.noreply.github.com>
2023-11-22 20:28:06 +0530

    Time: 6 ms (42.28%) | Memory: 42 MB (80.99%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int h[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            h[s.charAt(i)-'a']++;
+            h[t.charAt(i)-'a']--;
+        }
+        for(int x:h){
+            if(x!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
