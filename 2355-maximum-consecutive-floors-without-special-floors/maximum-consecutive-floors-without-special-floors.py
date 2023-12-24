a40e2273b4926473ad3b9fc4a6bce6de60fe35d1
Manoj KS <118463406+manojks6@users.noreply.github.com>
2023-12-24 15:52:47 +0530

    Time: 523 ms (88.89%) | Memory: 25.9 MB (16.67%) - LeetSync

--- 2355-maximum-consecutive-floors-without-special-floors/maximum-consecutive-floors-without-special-floors.py
+++ 2355-maximum-consecutive-floors-without-special-floors/maximum-consecutive-floors-without-special-floors.py
@@ -4,5 +4,5 @@ class Solution(object):
         m=max(special[0]-bottom,top-special[len(special)-1])
         for i in range(0,len(special)-1):
             m=max(m,special[i+1]-special[i]-1)
-        return m if m!=1 else 0
+        return m
         
