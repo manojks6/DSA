714701032a8ad5d9e9d12ee889c8477f4e167858
Manoj KS <118463406+manojks6@users.noreply.github.com>
2023-12-24 15:37:44 +0530

    Time: 13 ms (73.91%) | Memory: 13.2 MB (59.01%) - LeetSync

--- 2278-find-three-consecutive-integers-that-sum-to-a-given-number/find-three-consecutive-integers-that-sum-to-a-given-number.py
+++ 2278-find-three-consecutive-integers-that-sum-to-a-given-number/find-three-consecutive-integers-that-sum-to-a-given-number.py
@@ -2,20 +2,21 @@ class Solution(object):
     def sumOfThree(self, num):
         if num%3!=0:
             return []
-        if num==0:
-            return [-1,0,1]
-        l=0
-        h=num//3
-        while l<h:
-            m=(l+h)//2
-            ans=m*3+3
-            if ans==num:
-                return [m,m+1,m+2]
-            elif ans<num:
-                l=m+1
-            else:
-                h=m-1
-        return []
+        # if num==0:
+        #     return [-1,0,1]
+        # l=0
+        # h=num//3
+        # while l<h:
+        #     m=(l+h)//2
+        #     ans=m*3+3
+        #     if ans==num:
+        #         return [m,m+1,m+2]
+        #     elif ans<num:
+        #         l=m+1
+        #     else:
+        #         h=m-1
+        m=num//3
+        return [m-1,m,m+1]
 
 
 
