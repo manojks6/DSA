555f7a10f1e6c825edfb0c4c302e0dffc726d697
Manoj KS <118463406+manojks6@users.noreply.github.com>
2024-02-22 13:53:49 +0530

    Time: 2 ms (96.64%) | Memory: 44.3 MB (44.21%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -16,21 +16,23 @@
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if(subRoot==null) return true;
-        if(root==null) return false;
+        if(root==null){
+            return false;
+        }
         if(f(root,subRoot)){
             return true;
         }
-        return isSubtree(root.right,subRoot) || isSubtree(root.left,subRoot);
+        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
     }
-    static boolean f(TreeNode root, TreeNode subRoot){
-        if(root==null && subRoot==null){
+    static boolean f(TreeNode t, TreeNode s){
+        if(t==null && s==null){
             return true;
         }
-        if(root==null || subRoot==null){
+        if(t==null || s==null){
             return false;
         }
-        if(root.val==subRoot.val){
-            return f(root.right,subRoot.right) &&  f(root.left,subRoot.left) ;
+        if(s.val==t.val){
+            return f(t.left,s.left) && f(t.right,s.right);
         }
         return false;
     }
